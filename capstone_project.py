d78da12c913db9c4b5050d80b852770a81c57169
ShilpaGali34 <85815226+ShilpaGali34@users.noreply.github.com>
2021-06-15 14:14:28 +0530

    Add files via upload

---
+++ capstone_project.py
@@ -0,0 +1,10 @@
+import requests
+#import bs4
+
+#URL = 'https://www.amazon.in/dp/B08LRDM44F?pf_rd_r=K8XQ0ZA3MM83KJZ5MVSJ&pf_rd_p=537aa79e-9e4a-40b1-920d-f586be06410e'
+
+#headers = {"User-AgentMozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
+
+#page = requests.get(URL,headers = headers)
+
+#print(page)
